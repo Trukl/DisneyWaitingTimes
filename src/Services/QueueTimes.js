68ee1c817686d40989a10a091470092d4660bb0f
Corentin Minne <minnecoco@gmail.com>
2022-08-22 14:10:11 +0200

    fix(cors): try to fix cors

--- src/Services/QueueTimes.js
+++ src/Services/QueueTimes.js
@@ -1,8 +1,12 @@
 
 
 const getQueueTimes = async (id) => {
-  const response = await fetch(`https://queue-times.com/fr/parks/${id}/queue_times.json`);
+  const response = await fetch(`https://queue-times.com/fr/parks/${id}/queue_times.json`, {
+    // mode: 'no-cors',
+    crossDomain:true,
+  });
 
+  console.log(response)
   return response.json();
 };
 
