f785f59c81e2003fae76c7c6c4ae48907fcb7953
Corentin Minne <minnecoco@gmail.com>
2022-08-22 18:05:32 +0200

    fix(v0.1): remove unused import

--- src/Components/Attraction/Attraction.js
+++ src/Components/Attraction/Attraction.js
@@ -2,7 +2,6 @@ import './Attraction.css'
 import {Col} from "react-bootstrap";
 import {MdFavorite, MdFavoriteBorder} from "react-icons/md";
 import {useLocalStorage} from "../../useLocalStorage";
-import {CSSTransition} from 'react-transition-group';
 import {useEffect, useState} from "react";
 
 const getDurationClass = (time) => {
